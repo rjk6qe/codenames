bd039f9402e286308e28e2bd221f2d049bb1b440
Richard <rjk6qe@virginia.edu>
2017-06-11 21:10:01 -0400

    deleted unused js

--- generate_wordlist.py
+++ generate_wordlist.py
@@ -2,9 +2,12 @@ import csv
 import codenames.models as models
 from codenames import db
 
+
+db.create_all()
+
 with open('wordlist.csv', 'r') as f:
 	reader = csv.reader(f)
 	for row in reader:
 		word = models.Word('.'.join(row))
 		db.session.add(word)
-		db.session.commit()
+	db.session.commit()
